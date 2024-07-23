221983e31ae15c89256171fbbb3a0050d9b5e76b
Anto Kam <19kamh3@gmail.com>
2024-07-23 14:55:59 -0700

    fix(lab08): includes

--- lab08/ex2_test.c
+++ lab08/ex2_test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-#include "ex1.h"
+#include "ex2.h"
 
 /* -------------------------------Utilties, Do Not Modify------------------------------*/
 double* gen_array(int n) {
