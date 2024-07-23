60f9d246616c236483762d950874c62acf97e93f
Candice Yang <madaomax@berkeley.edu>
2024-07-22 22:14:46 -0700

    feat(lab08): release

---
+++ lab08/ex1.c
@@ -0,0 +1,21 @@
+#include "ex1.h"
+
+void v_add_naive(double* x, double* y, double* z) {
+    #pragma omp parallel
+    {
+        for(int i=0; i<ARRAY_SIZE; i++)
+            z[i] = x[i] + y[i];
+    }
+}
+
+// Adjacent Method
+void v_add_optimized_adjacent(double* x, double* y, double* z) {
+    // TODO: Implement this function
+    // Do NOT use the `for` directive here!
+}
+
+// Chunks Method
+void v_add_optimized_chunks(double* x, double* y, double* z) {
+    // TODO: Implement this function
+    // Do NOT use the `for` directive here!
+}
